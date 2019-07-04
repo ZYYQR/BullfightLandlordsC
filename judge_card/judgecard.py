172b1039b0bbebf5f38c8a604e3323ec2646a48f
flyingsopig <1193630409@qq.com>
2019-07-04 10:37:24 +0800

    add uuid(random) method

--- judge_card/judgecard.py
+++ judge_card/judgecard.py
@@ -1,3 +1,6 @@
+import uuid
+
+
 def judgeCardCategory():
     '''
     牌种类:
@@ -25,13 +28,20 @@ def SurePutCard(old_card_list,new_card_list):
     :param new_card_list: 我方出牌
     :return: 1: 可以出大上, 0 不可以出 牌型不符合,或者太小
     '''
+    card_dict = {
+        "J":"11","Q":"12","K":"13","A":"14","2":"15","XW":"16","DW":"17"
+    }
     card_category, card_connect_count,main_min_card = judgeCardCategory(old_card_list)
     card_category_new, card_connect_count_new,main_min_card_new = judgeCardCategory(old_card_list)
+    if main_min_card_new in card_dict.keys():
+        main_min_card_new = card_dict[main_min_card_new]
+    if main_min_card in card_dict.keys():
+        main_min_card = card_dict[main_min_card]
     if card_connect_count_new != 4 or (card_connect_count_new==4 and card_category==4):
-        if card_category_new == card_category and card_connect_count == card_connect_count_new and main_min_card < main_min_card_new:
+        if card_category_new == card_category and card_connect_count == card_connect_count_new and int(main_min_card) < int(main_min_card_new):
             print("压死")
             return 1
-        elif card_category_new == card_category and card_connect_count == card_connect_count_new and main_min_card >= main_min_card_new:
+        elif card_category_new == card_category and card_connect_count == card_connect_count_new and int(main_min_card) >= int(main_min_card_new):
             print("牌太小,出牌失败")
             return 0
         elif card_category_new != card_category or card_connect_count != card_connect_count_new :
@@ -41,4 +51,5 @@ def SurePutCard(old_card_list,new_card_list):
         print("压死")
         return 1
 
-
+def playUuid():
+    return uuid.uuid4()
